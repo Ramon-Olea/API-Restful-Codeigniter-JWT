ae84c01662c7503fbf0eb4b09075e8cc9abfc2d9
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-12-16 11:57:00 -0600

    api

---
+++ app/Helpers/jwt_helper.php
@@ -0,0 +1,40 @@
+<?php
+
+use Config\Services;
+use Firebase\JWT\JWT;
+use App\Models\UserModel;
+
+function getJWTFromRequest($authenticationHeader): string
+{
+    if (is_null($authenticationHeader)) {
+        throw new Exception('Missing or invalid JWT in request');
+    }
+
+    return explode(' ', $authenticationHeader)[1];
+}
+
+function validateJWTFromRequest(string $encodedToken)
+{
+    $key = Services::getSecretKey();
+    $decodedToken = JWT::decode($encodedToken, $key, ['HS256']);
+    $userModel = new UserModel();
+    $userModel->findUserByEmailAddress($decodedToken->email);
+}
+
+function getSignedJWTForUser(string $email): string
+{
+    $key = Services::getSecretKey();
+
+    $issuedAtTime = time();
+    $tokenTimeToLive = getenv('JWT_TIME_TO_LIVE');
+    $tokenExpiration = $issuedAtTime + $tokenTimeToLive;
+    $payload = [
+        'email' => $email,
+        'iat' => $issuedAtTime,
+        'exp' => $tokenExpiration
+    ];
+
+    $jwt = JWT::encode($payload, $key, 'HS256');
+
+    return $jwt;
+}
